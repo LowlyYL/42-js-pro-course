7c25d4721af5ebb08b112eba8796b6f5ee4644f7
[LowlyYl] <[artsiushko.yl@mail.ru]>
2021-04-19 11:14:29 +0300

    hw35

--- lessons/35-npm/public-package/index.js
+++ lessons/35-npm/public-package/index.js
@@ -1,6 +1,7 @@
-// :)
 const smartSearch = (array, value) => array.includes(value);
+const randomSearch = (value) => array[Math.floor(Math.random() * value)];
 
 module.exports = {
   smartSearch,
-}
+  randomSearch,
+};
